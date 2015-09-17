8f94597912b738cf07aafee254392f5d26874a62
Christopher Syers <csyers@nd.edu>
2015-09-17 00:29:44 -0400

    Submit PG1

--- PG1/README.md
+++ PG1/README.md
@@ -6,6 +6,34 @@ See `pg1.pdf` for the assignment.
 
 _Your complexity analysis and other notes here_
 
+Complexity analysis: The memory complexity of the implemetation of the undo feature in calc2, 
+which was implemented using a stack of stacks, is O(n^2), where n is the number of items added 
+to the calculator. This is because every time an item is added to the stack of numbers, the stack 
+as it looked before is pushed onto a stack of stack of doubles. This means that every number added 
+adds that number of elements to the stack of stacks. This results in 1+2+3+...+(n-1) for n pushes 
+onto the numbers stacks. Doing this sumation leads to n(n+1)/2 - 1 doubles that are added to the 
+stack. This reduces to O(n^2) space complexity.
+
+The memory complexity for the implementation of the memory (undo) feature in calc4, however would
+be O(n). This is because it simply creates one data structure, which creates one new node for every
+push onto it, or two new nodes for a swap operation. It does not need to store the whole stack in 
+memory, because of the use of shared pointers. In this way, the previous stack are preserved by having 
+multiple references to the the same nodes.
+
+In analyzing the space and time performance of the two implementations, calc4 performs a lot better. 
+When run with test4.sh, the calc2 cannot even complete the task. Running a "top" command to look at 
+the process statistics while it is running, it can take up to 21.0g of virtual memory, 90% of the CPUs, 
+and 94% of the computer's memory. It slows down quite the machines for everyone. One the other hand, 
+calc4 can successfully complete the test4.sh script, finishing in about .933 seconds, and only using 
+8.96 MB. Also, no memory is lost in calc4. When run with valgrind, all the blocks that were allocated 
+were freed, which is because of the shared pointers used in the implementation.
+
+NOTE: I did not actually use a shared_stack class object for the final implementation of the persistent 
+data structure. This implementation did work: however, it took slightly too much overhead to make a 
+shared_stack. When the code was switched to implement it with an stl stack, it reduced the memory usage 
+by enough to bring it below 10 MB. The stl stack contains shared_ptr to Node<doubles>. Node is defined in
+the shared_stack.h file, and it contains a constructor, a shared_ptr to another Node, and a data member of 
+type T. 
 
 _Grader comments here_
 
--- PG1/calc4.cpp
+++ PG1/calc4.cpp
@@ -1,19 +1,17 @@
 // Christopher Syers		CSE30331 Data Structures
 // Septempber 15, 2015		PG1
 
-// Implentation of a RPN calculator, using the My_stack class.
+// Implentation of a RPN calculator, using the stack class.
 // Calculator includes a memory, which is implemented using a
-// persistent data structure in the form of a vector of Node 
+// persistent data structure in the form of a stack of Node 
 // pointers
 
 #include <iostream>
 #include <sstream>
 #include <string>
 #include <unistd.h> // for isatty
-#include <stack>
-#include "My_stack.h" // for class My_stack
-#include <vector>
-
+#include <stack>	// for stack of node shared pointers
+#include "shared_stack.h" // for new node class
 using namespace std;
 
 // define a new type of enum, called operation
@@ -36,14 +34,13 @@ void error (const char *s) {
   }
 }
 
-// valid_size(stack<double>& numbers)
 // function that returns true if a stack contains 2 or more elements,
 // false otherwise 
-// precondition: numbers is a stack of doubles
+// precondition: memory is a stack of shared pointers to Node doubles
 // postcondition: true is returned is the size of the stack is 2 or greater, 
 // false is returned otherwise
-bool valid_size(My_stack<double>& numbers){
-  if(numbers.get_size() < 2){		// if size of stack is less than 2
+bool valid_size(stack<shared_ptr<Node<double>>>& memory){
+  if(memory.top()==nullptr || memory.top()->next == nullptr){	// if size of stack is less than 2
     return false;			// return false
   } else {
     return true;			// else, return true
@@ -53,16 +50,11 @@ bool valid_size(My_stack<double>& numbers){
 // do_operation(stack<double>& numbers, oper_t t)
 // function that does operation t on the top two elements of a stack
 // precondition: stack contains at least 2 elements
-// postcondition: the number returned is the result of the two numbers
-// with the given operation t, and those two numbers are removed from
-// the stack
-void do_operation(My_stack<double>& numbers, oper_t t,vector<Node<double>*>& memory){
-  Node<double>* back = memory.back();
-  back = back->next;
-  double x = numbers.top();	// get first element
-  numbers.pop();		// remove top element
-  double y = numbers.top();	// get second element
-  numbers.pop();		// remove top element
+// postcondition: the stack contains a new node that is the result of the operation
+// . this node is pushed onto the stack, and points to the value 2 prevous to it
+void do_operation(oper_t t,stack<shared_ptr<Node<double>>>& memory){
+  double x = memory.top()->data;	// get first element
+  double y = memory.top()->next->data;	// get second element
   double result;
   switch(t){			// switch on the enum to find operation
     case ADD:		
@@ -78,94 +70,57 @@ void do_operation(My_stack<double>& numbers, oper_t t,vector<Node<double>*>& mem
       result = y-x;
       break;
   }
-  Node<double>* new_node = new Node<double>(result);
-  new_node->next = back->next;
-  memory.push_back(new_node);
-  cout << result << endl;
-  numbers.push(result);
+  shared_ptr<Node<double>> new_node = make_shared<Node<double>>(result);	// new node to add, with value of result
+  new_node->next = memory.top()->next->next;					// new node points to the first element not used in operation
+  memory.push(new_node);							// new_node is pushed onto memory stack
+  cout << result << endl;							// the result is printed
 }
 
-// drop(stack<double>& numbers)
 // function that removes the top element in the stack. returns an error if 
 // the stack is empty
 // precondition: numbers is a stack of doubles
 // postcondition: top element is removed from stack, stack remains empty if it is already empty
-void drop(My_stack<double>& numbers, vector<Node<double>*>& memory){
-  if(numbers.get_size() == 0){		// if the stack is empty
+void drop(stack<shared_ptr<Node<double>>>& memory){
+  if(memory.size()==0 || memory.top() == nullptr){		// if the stack is empty
     error("stack underflow");		// return an error
     return;
   }
-  Node<double>* back = memory.back();
-  Node<double>* new_node = back->next;
-  memory.push_back(new_node);
-  numbers.pop();			// remove top element
+  shared_ptr<Node<double>> back = memory.top();			// pointer to the top element in class
+  shared_ptr<Node<double>> new_node = back->next;		// new node points to the same thing as the previous one does
+  memory.push(new_node);					// add the new node
 }
 
-// swap(stack<double>& numbers)
 // function that swaps the top two elements of a stack that is passed in
 // precondition: numbers is a stack of doubles
 // postcondition: top two elements are switched, or the stack is empty if
 // 		  there were fewer than 2 elements in the stack to begin
-void swap(My_stack<double>& numbers, vector<Node<double>*>& memory){
-  if(numbers.get_size() < 2){			// checks to see the size of the stack is less than 2
-    while(!numbers.empty()){			// loop to empty the array
-      numbers.pop();
-    }
-    memory.push_back(nullptr);
+void swap(stack<shared_ptr<Node<double>>>& memory){
+  if(memory.top() == nullptr || memory.top()->next == nullptr){			// checks to see the size of the stack is less than 2
+    memory.push(nullptr);			// makes the stack effectively empty
     error("stack underflow");			// return an error
     return;
   }
-
-  Node<double>* back = memory.back();
-  Node<double>* new_second = new Node<double>(back->data);
-  Node<double>* new_back = new Node<double>(back->next->data);
-  new_back->next = new_second;
-  new_second->next = back->next->next;
-  memory.push_back(new_back);
-  double first, second;				
-  first = numbers.top();			// get first element
-  numbers.pop();				// remove top element
-  second = numbers.top();			// get second element
-  numbers.pop();				// remove top element
-  numbers.push(first);				// push the stored values in the opposite order
-  numbers.push(second);	
+  shared_ptr<Node<double>> back = memory.top();			// get the last element of the stack
+  // make two new nodes that are equal in value to the last two in the stack
+  shared_ptr<Node<double>> new_second = make_shared<Node<double>>(back->data);		
+  shared_ptr<Node<double>> new_back = make_shared<Node<double>>(back->next->data);
+  new_back->next = new_second;			// back points to the second one
+  new_second->next = back->next->next;		// second one points to the element 2 before the previous last
+  memory.push(new_back);			// push the last node
 }
 
-// undo(stack<double>& numbers, stack<stack<double>>& memory)
 // function that undoes the previous operation by setting the stack equal
 // to the top element of the stack of stacks. It also removes the top element
 // from the stack of stacks
-// precondition: number is a stack of doubles and memory is a stack of
-// stacks of doubles
-// postcondition: stack is set to the top element of memory, and the top
-// element is removed from memory   
-void undo(My_stack<double>& numbers, vector<Node<double>*>& memory){
+// precondition: memory is a stack of shared pointers to double Nodes
+// postcondition: the head of the stack of the prevous stack is now the top element
+// in memory
+void undo(stack<shared_ptr<Node<double>>>& memory){
   if(memory.empty()){
     error("cannot undo");
     return;
   }
-  // empty the numbers stack
-  while(!numbers.empty()){
-    numbers.pop();
-  }
-  memory.pop_back();			// removes top element of memory
-  if(memory.empty()){			// if the memory is now empty, leave nubmers and memory empty
-    return;
-  }
-  // temporary stack of double
-  My_stack<double> temp;
-  // pointer to the back of the memory vector
-  Node<double>* cursor = memory.back();
-  // while there is something in the memory vector
-  while(cursor!=nullptr){
-    double new_element = cursor->data;	// get the data from that element
-    temp.push(new_element);		// push that element into a temporary stack
-    cursor = cursor->next;		// advance the pointer
-  }
-  while(!temp.empty()){			// loop through the temp stack
-    numbers.push(temp.top());		// push its elements onto the numbers stack
-    temp.pop();				// remove from the temporary stack
-  }
+  memory.pop();		// removes top element of memory
 }
 
 template <typename Number>
@@ -179,8 +134,7 @@ int string_to_number (const string &s, Number &x) {
 }
 
 int main (int argc, char *argv[]) {
-  My_stack<double> numbers;			// initialize the My_stack of doubles to store the operands
-  vector<Node<double>*> memory;			// a vector of node pointers that stores the memory
+  stack<shared_ptr<Node<double>>> memory;	// a stack of node pointers that stores the memory
   while (!cin.eof()) {
     if (interactive) cout << "> ";
     
@@ -192,71 +146,58 @@ int main (int argc, char *argv[]) {
     double x;	
     if (string_to_number(s, x) == 0) {        	// if the input was a number
       cout << x << endl;			// output x
-      numbers.push(x);
-      Node<double>* new_node = new Node<double>(x);
-      if(memory.empty()){
-	memory.push_back(new_node);
+      shared_ptr<Node<double>> new_node = make_shared<Node<double>>(x);		// make a new node with value x
+      if(memory.empty()){			// if the memory is empty,
+	memory.push(new_node);			// add the new node to the memory
       } else {
-        new_node->next = memory.back();
-        memory.push_back(new_node);
+        new_node->next = memory.top();		// make the new node point to the previous top
+        memory.push(new_node);			// make the new node the top
       }
     } else if (s == "") {
       // if nothing was passed into the program, do nothing
     } else {
       if (s=="*"){				// case for multiplication
-        if(valid_size(numbers)){		// if the stack is big enough,
-          do_operation(numbers,MULTIPLY,memory);	// multiply top 2 numbers
+        if(valid_size(memory)){			// if the stack is big enough,
+          do_operation(MULTIPLY,memory);	// multiply top 2 numbers
 	} else {
-          while(!numbers.empty()){
-            numbers.pop();
-          }
-	  memory.push_back(nullptr);
-          error("stack underflow");
+	  memory.push(nullptr);			// if there aren't enough elements to do an operation, push a null pointer
+          error("stack underflow");		// report an error
         }
       } else if (s=="+"){			// case for addition, same steps as above
-        if(valid_size(numbers)){
-          do_operation(numbers,ADD,memory);
+        if(valid_size(memory)){
+          do_operation(ADD,memory);
 	} else {
-          while(!numbers.empty()){
-            numbers.pop();
-          }
-	  memory.push_back(nullptr);
+	  memory.push(nullptr);
           error("stack underflow");
         }
       } else if (s=="/"){			// case for division, same steps as above
-        if(valid_size(numbers)){
-          do_operation(numbers,DIVIDE,memory);
+        if(valid_size(memory)){
+          do_operation(DIVIDE,memory);
 	} else {
-          while(!numbers.empty()){
-            numbers.pop();
-          }
-	  memory.push_back(nullptr);
+	  memory.push(nullptr);
           error("stack underflow");
         }
       } else if (s=="-"){			// case for subtraction, same steps as above
-        if(valid_size(numbers)){
-          do_operation(numbers,SUBTRACT,memory);
+        if(valid_size(memory)){
+          do_operation(SUBTRACT,memory);
 	} else {
-          while(!numbers.empty()){
-            numbers.pop();
-          }
-	  memory.push_back(nullptr);
+	  memory.push(nullptr);
           error("stack underflow");
         }
       } else if (s=="drop"){			// if the input was "drop," then call the drop function
-        drop(numbers,memory);
-        if(!numbers.empty()){			// if the last element was dropped, you cannot print the top element
-  	  cout << numbers.top() << endl;	// prints the top element in the stack
+        drop(memory);
+        if(!memory.empty() && memory.top()!=nullptr){
+	  cout << memory.top()->data << endl;	// print the top element in the new stack
         }
       } else if (s=="swap"){			// if the input was "swap," then call the swap function
-        swap(numbers,memory);	
-        if(!numbers.empty()){
-  	  cout << numbers.top() << endl;	// prints the top element in the stack
+        swap(memory);	
+        if(memory.top()!=nullptr){
+	  cout << memory.top()->data << endl;	// print the top elemetn in the new stack
         }
       } else if (s=="undo"){			// if the input is "undo", call the undo function
-        undo(numbers,memory);
-        if(!numbers.empty()){
-	  cout << numbers.top() << endl;	// print the top elemetn in the new stack
+        undo(memory);
+        if(!memory.empty() && memory.top()!=nullptr){
+	  cout << memory.top()->data << endl;	// print the top elemetn in the new stack
         }
       }
     }
