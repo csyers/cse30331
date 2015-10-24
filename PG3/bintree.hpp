5037d096da09c1f3fb85b7eff328be4105c107ee
Christopher Syers <csyers@nd.edu>
2015-10-24 11:28:39 -0400

    Submit PG3

--- PG3/bintree.hpp
+++ PG3/bintree.hpp
@@ -4,6 +4,32 @@
 #include <utility>
 #include <string>
 #include <vector>
+#include <algorithm> // for max
+
+// node struct for the bintree class
+struct Node{
+  double location;		// stores location in lineworld
+  std::string name;		// name of location
+  Node* left;			// pointer to left child
+  Node* right;			// pointer to right child
+  // function that fills the result vector with all children that fall within the search range
+  // precondition: this is not nullptr
+  // postcondition: result is filled with the names of nodes whose location is within s_min and s_max
+  void search(double s_min, double s_max, double b_min, double b_max, std::vector<std::string>& result){
+    if(std::max(s_min, b_min) <= std::min(s_max, b_max)){		// if brange and srange overlap
+      if(this->location >= s_min && this->location <= s_max){		// if location is within the srange
+        result.push_back(this->name);					// add the name to the vector
+      }
+      if (this->left != nullptr) this->left->search(s_min, s_max, b_min, this->location, result);	// recursive search down right, adjusting brange
+      if (this->right != nullptr) this->right->search(s_min, s_max, this->location, b_max, result);	// recursive search down left, adjusting brange
+    }
+  }
+  // destructor or the Node struct - removes the children recursively by calling delete
+  ~Node(){
+    delete right;
+    delete left;
+  }
+};
 
 class bintree {
   // A binary search tree for locations in Lineland.
@@ -11,10 +37,13 @@ class bintree {
   // Notes: 
   // - Assume a flat, one-dimensional world with locations from -180 to 180.
   // - All locations and distances are measured in the same units (degrees).
+private:
+  Node* root;		// stores pointer to the root Node
 
 public:
   // Default constructor
   bintree() {
+    root = nullptr;	// set data member to nullptr
   }
 
   // Copy constructor
@@ -22,10 +51,26 @@ public:
     // Copy all data members here. It should normally be a deep copy,
     // i.e., the things that pointers point to should be copied as
     // well.
+    root = copyTree(t.root);
   }
 
+  // helper function for copy constructor
+  // precondition: none
+  // postcondition: returns a Node* to a bintree that has the same elements as other
+  Node* copyTree(Node* other){
+    if(other==nullptr){			// return nullptr if there is no Node to copy
+      return nullptr;
+    }
+    Node* newNode = new Node();		// create a new node
+    newNode->location = other->location;	// copy the location and name of other node
+    newNode->name = other->name;
+    newNode->left = copyTree(other->left);	// make the newNode's left pointer the same as the old node's
+    newNode->right = copyTree(other->right);	// recursive with the right subtree as well
+    return newNode;			
+  }
   // Destructor
   ~bintree() {
+    delete root;
   }
 
   // Copy assignment is implemented using the copy-swap idiom
@@ -35,6 +80,7 @@ public:
     // Swap all data members here, e.g.,
     // swap(t1.foo, t2.foo);
     // Pointers should be swapped -- but not the things they point to.
+    swap(t1.root, t2.root);		// swap the data members
   }
   bintree &operator= (bintree other) {
     // You don't need to modify this function.
@@ -44,6 +90,28 @@ public:
 
   void insert(const std::string& name, double p) {
     // Insert an element with name `name` and location `p`.
+    Node* insert_node = new Node();	// make a new Node
+    insert_node->name = name;		// set the new Node's data members to the parameters
+    insert_node->location = p;
+    insert_node->left = nullptr;	// start the new Node's pointers to null
+    insert_node->right = nullptr;
+    if(root==nullptr){			// if the root is null, just insert the node as the root
+      root = insert_node;
+    } else {				// if the root is not null:
+      Node *current, *parent;
+      current = root;
+      parent = root;
+      current = (insert_node->location < current->location) ? (current->left) : (current->right);	// move current to the correct side of the root: left if less than, right if greater than or equal to
+      while(current!=nullptr){		// continue until current is null
+	parent = current;		// move parent to the current node
+        current = (insert_node->location < current->location) ? (current->left) : (current->right);	// advance current to the correct side of node
+      }
+      if(insert_node->location < parent->location){		// insert the node to the left if it is less than, right if greater than or equal
+        parent->left = insert_node;
+      } else {
+        parent->right = insert_node;
+      }
+    }
   }
 
   void within_radius(double p, double r, std::vector<std::string> &result) const {
@@ -52,6 +120,8 @@ public:
     // Postcondition: `result` contains all (and only) elements of the
     // tree, in any order, that lie within the range `p` plus or minus
     // `r`.
+    result.clear();				// clear the current results
+    root->search(p-r,p+r,-180,180,result);	// use Node's search to populate results vector
   }
 };
 
--- PG3/quadtree.hpp
+++ PG3/quadtree.hpp
@@ -4,36 +4,190 @@
 #include <utility>
 #include <string>
 #include <vector>
-
 #include "distance.hpp"
+
+// struct to hold the nodes of the quadtree
+struct Node{
+  std::string name;	// holds the name of the location
+  point location;	// holds a point that contains that lat and lon of a location
+  // pointers to four children
+  Node* southwest;	
+  Node* southeast;
+  Node* northwest;
+  Node* northeast;
+  // function that determines all locations that are within r or point center
+  // precondition: none
+  // postcondition: result contains the names of all loactions of the caller's subtree that are 
+  // within r of point center
+  void search(point center, double r, point sw, point ne, std::vector<std::string>& result){
+    if(distance_to_box(center, sw, ne) <= r){		// if the search range and bounding box intersect
+      if(distance(center,location) <= r) {		// if the location is within the search range
+        result.push_back(name);				// add the name to the vector
+      }
+      // recursively search down each branch, restricting the bounding range of the search
+      if (southwest != nullptr) southwest->search(center,r,sw,location,result);
+      if (southeast != nullptr) southeast->search(center,r,point(sw.lat,location.lon),point(location.lat,ne.lon),result);
+      if (northwest != nullptr) northwest->search(center,r,point(location.lat,sw.lon),point(ne.lat,location.lon),result);
+      if (northeast != nullptr) northeast->search(center,r,location,ne,result);
+    }
+  }
+  // destructor of a Node struct. Recursively remove the children, which calls their destructors as well
+  ~Node(){
+    delete southwest;
+    delete southeast;
+    delete northwest;
+    delete northeast;
+  }
+};
 
 class quadtree {
   // A point quadtree for locations on Earth.
 
   // Notes: 
   // - Assume a spherical Earth. But assume that if you keep traveling
-  //   west, you will hit longitude -180 and stop, and if you keep
-  //   traveling east, you will hit longitude +180 and stop; you don't
+  //   west, you will hit lon -180 and stop, and if you keep
+  //   traveling east, you will hit lon +180 and stop; you don't
   //   have to worry about wrapping around like Magellan.
-  // - Locations are measured in degrees latitude and longitude, but
+  // - Locations are measured in degrees lat and lon, but
   //   distances are measured in miles.
-
+private:
+  Node* root;
 public:
   // See bintree.hpp for skeleton definitions of the default
   // constructor, copy constructor, destructor, and copy assignment
   // operator (and swap).
+  
+  // constructor
+  // initializes the root pointer to null
+  quadtree(){
+    root = nullptr;
+  }
 
+  // copy constructor
+  // precondition: none
+  // postconidition: root is the root of a tree with the same elements as t, the quadtree to copy
+  quadtree(const quadtree &t){
+    root = copyTree(t.root);	// calls helper function copyTree
+  }
+
+  // helper function for copying a tree
+  // precondition: none
+  // postcondition: returns a pointer to a copy of a node other
+  Node* copyTree(Node* other){
+    if(other==nullptr){			// return nullptr if other is nullptr
+      return nullptr;
+    }
+    Node* newNode = new Node();				// create the new node
+    // copy the information from the other node into the newNode
+    newNode->name = other->name;			
+    newNode->location.lat = other->location.lat; 
+    newNode->location.lon = other->location.lon;
+    // recursively copy the children of the other node
+    newNode->southwest = copyTree(other->southwest);
+    newNode->southeast = copyTree(other->southeast);
+    newNode->northwest = copyTree(other->northwest);
+    newNode->northeast = copyTree(other->northeast);
+    // return the newNode
+    return newNode;
+  }
+
+  // destructor. 
+  // precondition: none
+  // postcondition: root and all of its children as destroyed 
+  ~quadtree(){
+    delete root;
+  }
+
+  // swap helper function
+  friend void swap(quadtree &t1, quadtree &t2){
+    using std::swap;
+    swap(t1.root, t2.root); 	// swap the roots of the two quadtrees
+  }
+ 
+  // overloaded = operator. Uses copy swap idiom
+  quadtree &operator= (quadtree other){
+    swap(*this,other);
+    return *this;
+  }
+
+  // function to insert new location into quadtree
+  // precondition: point p is within (-90,-180) and (90,180)
+  // postcondition: a new Node is inserted into the quadtree
+  // in the correct location
   void insert(const std::string& name, point p) {
     // Insert an element with name `name` and location `p`.
+    Node* insert_node = new Node();		// create the new Node struct
+    // initialize the data members of the Node
+    insert_node->name = name;
+    insert_node->location = p;
+    insert_node->southwest = nullptr;
+    insert_node->southeast = nullptr;
+    insert_node->northwest = nullptr;
+    insert_node->northeast = nullptr;
+    // if this is the first insertion, make the new node the root
+    if(root==nullptr){
+      root = insert_node;
+    } else {
+      Node *current, *parent;		// cursor node pointers
+      // start both at the root
+      current = root;
+      parent = root;
+      if(insert_node->location.lon < current->location.lon){		// if the point is west of the current node
+        if(insert_node->location.lat < current->location.lat){		// if the point is south of the current node
+          current = current->southwest;						// move cursor to the southwest child
+        } else {							// if the point is north of the current node
+          current = current->northwest;						// move cursor to the northwest chile
+        }
+      } else {								// if the point is east of the current node
+        if(insert_node->location.lat < current->location.lat){		// if the point is south of the current node
+          current = current->southeast;						// move cursor to the southeast child
+        } else {							// if the point is north of the current node
+          current = current->northeast;						// move cursor to the northeast child
+        }
+      }
+      while(current!=nullptr){						// continue until the process reaches a nullptr that could be added to
+        parent = current;						// move parent pointer to the current node
+	// move current to the correct child - depending on location of insert_node compared to the current node being looked at
+        if(insert_node->location.lon < current->location.lon){
+          if(insert_node->location.lat < current->location.lat){
+            current = current->southwest;
+          } else {
+            current = current->northwest;
+          }
+        } else {
+          if(insert_node->location.lat < current->location.lat){
+            current = current->southeast;
+          } else {
+            current = current->northeast;
+          }
+        }
+      }
+      // insert the node into the correct place based on the relative location of the insert_node and the parent node
+      if(insert_node->location.lon < parent->location.lon){
+        if(insert_node->location.lat < parent->location.lat){
+          parent->southwest = insert_node;				// insert southwest if lon&lat of insert_node is < lon&lat of parent
+        } else {
+          parent->northwest = insert_node;				// insert northwest if insert lon < parent lon & insert lat >= parent lat
+        }
+      } else {
+        if(insert_node->location.lat < parent->location.lat){
+          parent->southeast = insert_node;				// insert southeast if insert lon >= parent lon & insert lat < parent lat
+        } else {
+	  parent->northeast = insert_node;				// insert northeast if lon&lat of insert_node is >= lon&lat of parent
+	}
+      }
+    }
   }
 
   void within_radius(point p, double r, std::vector<std::string> &result) const {
     // Search for elements within the circle with center `p` (in
-    // degrees latitude and longitude) and radius `r` (in miles).
+    // degrees lat and lon) and radius `r` (in miles).
     // Clears `result` and puts the elements in `result`.
     // Postcondition: `result` contains all (and only) the elements of
     // the tree, in any order, that lie within the circle defined by
     // `p` and `r`.
+    result.clear();						// clears the results vector
+    root->search(p,r,point(-90,-180),point(90,180),result);	// starts the recursive search at the root, with bounding range of the whole coordinate system
   }
 };
 
